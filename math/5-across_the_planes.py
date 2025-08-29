0a9f42c4d3468b9fc62f40819e367fa7ba0cc908
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 11:43:32 +1000

    5 bis

--- math/5-across_the_planes.py
+++ math/5-across_the_planes.py
@@ -3,6 +3,8 @@ matrix_shape = __import__('2-size_me_please').matrix_shape
 
 def add_matrices2D(mat1, mat2):
     
+    if not mat1 or not mat2:
+        return None
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     
