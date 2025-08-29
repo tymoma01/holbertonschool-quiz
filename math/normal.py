04d70c1f16a51426591019ab2875546185b1a990
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 12:24:38 +1000

    9

---
+++ math/normal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+
+class Normal:
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = np.mean(np.array(data))
+            self.stddev = np.std(np.array(data))
