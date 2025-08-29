2970988b9c24ce2fd55468271a22c8251bb90b8e
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 11:26:26 +1000

    5

---
+++ math/5-across_the_planes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+matrix_shape = __import__('2-size_me_please').matrix_shape
+
+def add_matrices2D(mat1, mat2):
+    
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    
+    new_mat = []
+    for i in range(len(mat1)):
+        row = []
+        for j in range(len(mat2)):
+            row.append(mat1[i][j] + mat2[i][j])
+        new_mat.append(row)
+    return new_mat
