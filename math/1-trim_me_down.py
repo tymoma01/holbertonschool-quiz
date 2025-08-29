788e9fe5defe08a1762b99309ed9e4c421229c4c
Thomas YAMINI <t.yamini@groupeonepoint.com>
2025-08-29 10:57:13 +1000

    1

--- math/1-trim_me_down.py
+++ math/1-trim_me_down.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
-the_middle = [item[2:4] 
-              for item in matrix]
-
+the_middle = []
+for item in matrix:
+    the_middle.append(item[2:4])
 print("The middle columns of the matrix are: {}".format(the_middle))
